123655d72f87998eabacfa314b51c8997b15788f
Keitiankor <keitian.kor@gmail.com>
2023-06-12 20:52:34 +0900

    fix

--- src/main/java/com/example/springbootwithvue/study/studycontroller/StudyController.java
+++ src/main/java/com/example/springbootwithvue/study/studycontroller/StudyController.java
@@ -4,5 +4,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hi")
 public class StudyController {}
